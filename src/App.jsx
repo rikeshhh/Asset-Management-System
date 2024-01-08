69179fb650f7db61239f8b94e46fab960a772999
SHIRISH <shirish.shrestha07@gmail.com>
2024-01-08 13:40:56 +0545

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
- 
-
   return (
     <>
-    hi
+      <h1>shirish</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
