59f811afb6e30d603297c81ae5786d8ed3ccb30d
Rikesh Shrestha <rikeshmania123@gmail.com>
2024-01-09 01:50:58 +0545

    table design

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Signup } from "./Components/Signup/Signup";
+import { Table } from "./Components/Hero/Table/Table";
 
 function App() {
   return (
     <>
-    
+  <Table/>
     </>
   );
 }
---
+++ src/Components/Signup/Signup.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+import './Signup.css'
+export const Signup = () => {
+    const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+    return (
+        <section className='main-container signup'>
+            {/* <div className='user__auth'>
+                <div>
+                    <h2 className='user__auth--heading'>AMS</h2>
+                    <div className=''>
+                        <p>Asset Management System</p>
+                    </div>
+                </div>
+                <form action="" className='user__auth--form'>
+                    <h3>Signup</h3>
+                    <div>
+                        <label htmlFor="username">Username</label>
+                        <input type="text" placeholder='Enter your username' />
+                        <label htmlFor="email">Email</label>
+                        <input type="email" placeholder='Enter your username' />
+                        <label htmlFor="password">Password</label>
+                        <input placeholder='Enter your password'
+                            type={
+                                showPassword ? "text" : "password"
+                            }
+                            value={password}
+                            onChange={(e) =>
+                                setPassword(e.target.value)
+                            }
+                        />
+                        <label for="retype">Retype password: </label>
+                        <input
+                            id="pass"
+                            type={
+                                showPassword ? "text" : "password"
+                            }
+                            value={password}
+                            onChange={(e) =>
+                                setPassword(e.target.value)
+                            }
+                        />
+
+                        <label for="check">Show Password</label>
+                        <input
+                            id="check"
+                            type="checkbox"
+                            value={showPassword}
+                            onChange={() =>
+                                setShowPassword((prev) => !prev)
+                            }
+                        />
+                    </div>
+                    <div>
+                        <p>already have an account?<span>Login</span></p>
+                    </div>
+                    <button>Signup</button>
+                </form>
+                <div className='user__auth__ques'>
+                    <span>Please contact the admin at admin@ams.com for help</span>
+                </div>
+            </div> */}
+        </section>
+    )
+}
