25cb4acc900d50eddcc918c85fa93061153f3f82
SHIRISH <shirish.shrestha07@gmail.com>
2024-01-11 14:02:09 +0545

    added icons to the navbar

--- src/Pages/Header/Header.jsx
+++ src/Pages/Header/Header.jsx
@@ -15,39 +15,66 @@ const Header = () => {
             System
           </h4>
           <ul className="navbar__list">
-            <Link to="/">
-              <li>Dashboard</li>
+            <Link to="/" className="navbar__link">
+              <li>
+                <img src="/src/assets/dashboard.svg" alt="" />
+                Dashboard
+              </li>
             </Link>
-            <Link to="/assets">
-              <li>Assets</li>
+            <Link to="/assets" className="navbar__link">
+              <li>
+                <img src="/src/assets/assets.svg" alt="" />
+                Assets
+              </li>
             </Link>
-            <Link to="/procurement">
-              <li>Procurement</li>
+            <Link to="/procurement" className="navbar__link">
+              <li>
+                <img src="/src/assets/procurement.svg" alt="" />
+                Procurement
+              </li>
             </Link>
-            <Link to="/repair">
-              <li>Repair & Replace</li>
+            <Link to="/repair" className="navbar__link">
+              <li>
+                <img src="/src/assets/repair.svg" alt="" />
+                Repair & Replace
+              </li>
             </Link>
           </ul>
         </div>
         <div className="bottom__list">
           <h4 className="navbar__title">Listing</h4>
           <ul className="navbar__list">
-            <Link to="/employees">
-              <li>Employees</li>
+            <Link to="/employees" className="navbar__link">
+              <li>
+                <img src="/src/assets/employee.svg" alt="" />
+                Employees
+              </li>
             </Link>
-            <Link to="/categories">
-              <li>Categories</li>
+            <Link to="/categories" className="navbar__link">
+              <li>
+                <img src="/src/assets/categories.svg" alt="" />
+                Categories
+              </li>
             </Link>
-            <Link to="/location">
-              <li>Locations</li>
+            <Link to="/location" className="navbar__link">
+              <li>
+                <img src="/src/assets/location.svg" alt="" />
+                Locations
+              </li>
             </Link>
-            <Link to="/departments">
-              <li>Department</li>
+            <Link to="/departments" className="navbar__link">
+              <li>
+                <img src="/src/assets/department.svg" alt="" />
+                Department
+              </li>
             </Link>
 
             <div className="plans"></div>
-            <Link to="">
-              <li>Logout</li>
+            <Link to="" className="navbar__link">
+              <li>
+                <img src="/src/assets/logout.svg" alt="" />
+                Logout
+              </li>
             </Link>
           </ul>
         </div>
