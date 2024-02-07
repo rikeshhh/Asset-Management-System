81e193cd809cac65910bb0c52255f129852f77e4
Rikesh Shrestha <rikeshmania123@gmail.com>
2024-02-07 21:34:30 +0545

    conflict resolved

--- src/Pages/Departments/DepartmentDataTable.jsx
+++ src/Pages/Departments/DepartmentDataTable.jsx
@@ -108,7 +108,7 @@ const [departmentId,setDepartmentId] = useState("");
                 <td>{index + 1}</td>
                 {departmentId===options.id &&show ? (
                   <td className={show?"universal__td--border" : ""}>
-                  <form onSubmit={handleSubmit(onUpdateData)} className="Universal__update--form">
+                  <form onSubmit={handleSubmit(onUpdateData)} className="universal__update--form">
                     <InputField
                       name="department"
                       register={register}
