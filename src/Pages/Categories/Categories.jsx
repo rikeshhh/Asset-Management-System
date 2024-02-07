6e339f0f449fa4aa333299bd5ea30535511a564d
SHIRISH <shirish.shrestha07@gmail.com>
2024-02-07 13:50:26 +0545

    category add delete edit fetch feature add

--- src/Pages/Categories/Categories.jsx
+++ src/Pages/Categories/Categories.jsx
@@ -1,47 +1,56 @@
-import { useForm, Controller } from "react-hook-form";
+import "./Categories.css";
+import { useForm } from "react-hook-form";
 import { DataTable } from "../../Component/DataTable/DataTable";
 import { InputField } from "../../Component/Input/InputField";
 import { Label } from "../../Component/Label/Label";
 import { SelectInput } from "../../Component/Input/SelectInput";
-import "./Categories.css";
 import Button from "../../Component/Button/Button";
 import Model from "../../Component/Model/Model";
-import { useState } from "react";
 import { IoMdAdd } from "react-icons/io";
+import { categoryAdd, getCategoryData } from "./CategoryApiSice";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import CategoryDataTable from "./CategoryDataTable";
 
 const Categories = () => {
-  const CategoryOptions = [
-    {
-      key:1,
-      value:'Computer'
-    },
-    {
-      key:2,
-      value:'Backup'
-    },
-    {
-      key:3,
-      value:'Accessories'
-    },
-    {
-      key:4,
-      value:'Router'
-    }
-];
-const options=["Frontend"]
+  const options = ["Frontend"];
   const {
     register,
     formState: { errors },
     handleSubmit,
-    control,
     reset,
   } = useForm();
 
+  const {
+    isPending,
+    error,
+    data: CategoryData,
+  } = useQuery({
+    queryKey: ["CategoryData"],
+    queryFn: getCategoryData,
+  });
 
-  const onSubmit = (data) => {
-    console.log(data.ChildCategory);
-    console.log(data.ParentCategory);
+  const onCategorySubmit = (data) => {
+    addLocation.mutate(data);
   };
+
+  const addLocation = useMutation({
+    mutationFn: (formData) => {
+      return categoryAdd(formData.parent);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries("CategoryData");
+      reset();
+    },
+    onError: (error) => {
+      if (error.response.status === 401) {
+        console.log("Unauthorized: Please log in with valid id.");
+      }
+    },
+  });
+
+  if (isPending) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
   return (
     <section className="content-wrapper">
       <div className="content-radius category">
@@ -49,9 +58,7 @@ const options=["Frontend"]
           <h2>Categories</h2>
         </div>
         <div className="category__content">
-          <DataTable
-           CategoryOptions={CategoryOptions}
-          />
+          <CategoryDataTable CategoryData={CategoryData} />
 
           <div className="add__category">
             <div className="add__category--title">
@@ -61,7 +68,7 @@ const options=["Frontend"]
                 parent category.
               </span>
             </div>
-            <form action="" onSubmit={handleSubmit(onSubmit)}>
+            <form action="" onSubmit={handleSubmit(onCategorySubmit)}>
               <div>
                 <Label sup={"*"} text="Name" />
                 <InputField
@@ -77,14 +84,7 @@ const options=["Frontend"]
               </div>
               <div className="add__category--select">
                 <Label sup={"*"} text="Parent Category" />
-                <Controller
-                  name="ParentCategory"
-                  control={control}
-                  defaultValue="" // Set the default value
-                  render={({ field }) => (
-                    <SelectInput {...field} options={options} />
-                  )}
-                />
+                <SelectInput options={options} />
               </div>
               <div className="">
                 <Button
