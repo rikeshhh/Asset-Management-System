904e7da2ac12f07122afd8f20b6666f65a62a998
asbin12 <140784814+asbin12@users.noreply.github.com>
2024-02-09 12:30:32 +0545

    changes

--- src/Component/PendingTable/PendingTableBody.jsx
+++ src/Component/PendingTable/PendingTableBody.jsx
@@ -1,37 +1,37 @@
 import Skeleton from "react-loading-skeleton";
-import 'react-loading-skeleton/dist/skeleton.css'
+import "react-loading-skeleton/dist/skeleton.css";
 const PendingTableBody = () => {
   return (
     <>
-      {
-        Array.from({ length: 20 }).map((_, index) => (
-          <tr key={index}>
-            <td>
-              <Skeleton width={127} height={10} />
-            </td>
-            <td>
-              <Skeleton width={209} height={10} />
-            </td>
-            <td>
-              <Skeleton width={100} height={10} />
-            </td>
-            <td>
-              <Skeleton width={84} height={10} />
-            </td>
-            <td>
-              <Skeleton width={131} height={10} />
-            </td>
-            <td>
-              <Skeleton width={130} height={10} />
-            </td>
-            <td>
-              <Skeleton width={125} height={10} />
-            </td>
-          </tr>
-        ))
-      }
+      {Array.from({ length: 20 }).map((_, index) => (
+        <tr key={index}>
+          <td>
+            <Skeleton width={127} height={10} />
+          </td>
+          <td>
+            {" "}
+            <Skeleton width={209} height={10} />{" "}
+          </td>
+          <td>
+            <Skeleton width={100} height={10} />
+          </td>
+          <td>
+            <Skeleton width={84} height={10} />
+          </td>
+          <td>
+            <Skeleton width={131} height={10} />
+          </td>
+          <td>
+            {" "}
+            <Skeleton width={130} height={10} />
+          </td>
+          <td>
+            <Skeleton width={125} height={10} />
+          </td>
+        </tr>
+      ))}
     </>
-  )
-}
+  );
+};
 
-export default PendingTableBody
+export default PendingTableBody;
