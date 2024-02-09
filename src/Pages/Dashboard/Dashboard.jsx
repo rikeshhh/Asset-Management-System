35cfde60e6f6e67e643e9e73f9950b218476035c
Rikesh Shrestha <rikeshmania123@gmail.com>
2024-02-09 13:24:35 +0545

    links maintained

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -7,10 +7,11 @@ const Dashboard = () => {
       <h3 className="ams__Dashboard">AMS DASHBOARD</h3>
       <p className="work__on--progress">Work On Progress</p>
       <span className="dashboard--span">Please continue checking other ui pages</span>
+        <Link to="/assets" className="link">
       <Button className='button__blue' text="Go To Assets">
-        <Link to="/assets">
-        </Link>
+
       </Button>
+        </Link>
     </section>
   );
 };
