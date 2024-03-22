bee97c03b1e2799865f9883583e1e3911a6815a3
SHIRISH <shirish.shrestha07@gmail.com>
2024-03-22 14:08:31 +0545

    feat: delete procurement

--- src/Pages/Employees/Employees.jsx
+++ src/Pages/Employees/Employees.jsx
@@ -107,9 +107,6 @@ const Employees = () => {
     },
     onError: (error) => {
       notifyError(error.message);
-      if (error.response.status === 401) {
-        notifyError("Unauthorized: Please log in with valid id.");
-      }
     },
   });
 
