f51d76ac476d3d19c85ae0e11c9922334a29c2ef
SHIRISH <shirish.shrestha07@gmail.com>
2024-04-03 12:49:22 +0545

    fix: image issue repair replace

--- src/Pages/Repair/ViewRepair.jsx
+++ src/Pages/Repair/ViewRepair.jsx
@@ -45,7 +45,7 @@ const ViewRepair = () => {
           <span>Fill this form for repair or replace of your device</span>
         </div>
         <div className="user__profile--body">
-          <div className="user__repair--left ">
+          <div className="user__profile--image repair__view--image ">
             <Label text="View current asset image" />
             <ImagePath
               file={tableData.product_image}
